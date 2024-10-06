2235bc3857e94e82244895e099f7eb46d4fbe4ef
김상두 <puki4416@gmail.com>
2024-10-06 18:46:10 +0900

    feat: about 경력, opensource 항목 개선

--- src/components/about/careers/careers.tsx
+++ src/components/about/careers/careers.tsx
@@ -8,8 +8,17 @@ const Careers = () => {
       <h2 className={styles.title}>이력</h2>
       <ul className={styles.listWrap}>
         <li>
-          <strong>테크타카</strong>
-          <div> Frontend Engineer (2023. 06 ~ 현재)</div>
+          <div  className={styles.listHeader}>          
+            <h3>테크타카</h3>
+            <p>2023. 06 ~ 현재</p>
+          </div>
+          <ul className={styles.listContents}>
+            <li>WMS, Fullfilment 페이지 프론트엔드 개발</li>
+            <li>웹페이지 국제화 적용시 상수화를 쉽게 할수 있도록 스크립트를 개발하여 번역에 걸리는 시간 단축</li>
+            <li>아이콘 피그마 플러그인, 자동화 스크립트, 아이콘 라이브러리를 제작하여 복잡한 아이콘 사용방식 개선</li>
+            <li>멀티레포로 구성된 페이지를 모노레포로 구성하여 관심사의 집중</li>
+            <li>팀내 컨벤션 정리후 필요한 경우 custom rule까지 활용하여 컨벤션 관리 자동화</li>
+          </ul>
         </li>
       </ul>
     </section>
--- src/components/about/openSource/openSource.tsx
+++ src/components/about/openSource/openSource.tsx
@@ -1,21 +1,85 @@
 import * as React from "react"
 import * as styles from "./openSource.module.css"
-
+import data from "./constant"
 const OpenSource = () => {
+//   const [list,setList] = React.useState({})
+//   React.useEffect(()=>{
+//     const username = "developer-bandi";  // GitHub 사용자 이름
+// const token = ""; // 발급받은 Personal Access Token
+
+// // GitHub API URL
+// const apiUrl = `https://api.github.com/search/issues?q=type:pr+author:${username}&per_page=100`;
+
+// fetch(apiUrl, {
+//   method: "GET",
+//   headers: {
+//     "Authorization": `token ${token}`,  // 인증 토큰 추가
+//     "Accept": "application/vnd.github.v3+json"  // GitHub API 버전 명시
+//   }
+// })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();  // JSON 형태로 변환
+//   })
+//   .then(data => {
+    
+
+//     const a = data.items.reduce((prev,{repository_url,title,pull_request:{url}})=>{
+//       const element = {
+//         title,
+//         url
+//       }
+
+//       if(prev[repository_url] !== undefined){
+//         prev[repository_url].push(element)
+//       }
+
+//       return prev
+//     },{
+//       "https://api.github.com/repos/chaijs/chai":[],
+//       "https://api.github.com/repos/import-js/eslint-plugin-import":[],
+//       "https://api.github.com/repos/jsx-eslint/eslint-plugin-react":[],
+//       "https://api.github.com/repos/typescript-eslint/typescript-eslint":[],
+//       "https://api.github.com/repos/DefinitelyTyped/DefinitelyTyped":[],
+//       "https://api.github.com/repos/react-hook-form/react-hook-form":[]
+//     })
+
+//     console.log(JSON.stringify(a))
+//     setList(a)
+//   })
+//   .catch(error => {
+//     console.error("Error fetching data:", error);
+//   });  
+
+//   },[])
+
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>오픈소스 컨트리뷰트</h2>
+      <p>아직 걸음마 단계이지만 관심있는 오픈소스, 특히 AST 관련 프로젝트에 주로 기여하고있습니다. 아래에서 프로젝트별로 기여한 pr을 확인하실수 있으며 제목을 누르면 실제 pr을 확인해 보실수 있습니다.</p>
       <ul className={styles.repositoryListWrap}>
-        <li className={styles.repositoryItem}>
-          <h3 className={styles.repositoryTitle}>eslint-plugin-react</h3>
+        {Object.entries(data).sort((a,b)=>b[1].length-a[1].length).map(([key,value])=>{
+          const repoName = key.split("/")[key.split("/").length-1]
+
+          return (
+<li className={styles.repositoryItem}>
+          <h3 className={styles.repositoryTitle}>{repoName}</h3>
           <ul className={styles.contributeListWrap}>
-            <li className={styles.contributeItem}>
-              <a href="https://github.com/jsx-eslint/eslint-plugin-react/pull/3656">
-                jsx-key 규칙 문서의 문법 오류 수정
-              </a>
-            </li>
+            {value.map(({title,url})=>{
+            return (<li className={styles.contributeItem}>
+            <a href={url}>
+              {title}
+            </a>
+          </li>)
+            })}
           </ul>
         </li>
+          )
+
+        })}
       </ul>
     </section>
   )
